c6d29c52d21209b0aecb4b9628c357c0150a0985
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-22 08:46:15 +0200

    consuming promises

--- script.js
+++ script.js
@@ -72,6 +72,8 @@ const getCountryAndNeighbour = function (country) {
   );
   request.send();
 
+  //// CALLBACK HELL
+
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
@@ -96,14 +98,47 @@ const getCountryAndNeighbour = function (country) {
 getCountryAndNeighbour('usa');
 */
 
-// const request = new XMLHttpRequest();
-// request.open(
-//   'GET',
-//   `https://countries-api-836d.onrender.com/countries/name/${country}`
-// );
-// request.send();
+// Promises: ES6 feature
 
-const request = fetch(
-  `https://countries-api-836d.onrender.com/countries/name/spain`
-);
-console.log(request);
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.subregion}</h4>
+          <p class="country__row"><span>👫</span>${(
+            data.population / 1000000
+          ).toFixed(1)} mil. people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+/*
+const getCountryData = function (country) {
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+};
+*/
+
+// CONSUMING PROMISES // callback without callback hell
+
+const getCountryData = function (country) {
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('czech republic');
