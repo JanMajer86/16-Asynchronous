3eed2dbc1dae6d2af36446a5943a14cfed90661f
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-26 18:39:32 +0200

    016 Building a Simple Promise

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 
 // const getCountryData = function (country) {
 //   // Country 1
-//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//
 //     .then(response => {
 //       console.log(response);
 
@@ -69,6 +69,7 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 //     });
 // };
 
+/*
 const getCountryData = function (country) {
   // Country 1
   getJSON(
@@ -98,3 +99,44 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('australia');
 });
+
+*/
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening 🔮');
+
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(y => console.error(y));
